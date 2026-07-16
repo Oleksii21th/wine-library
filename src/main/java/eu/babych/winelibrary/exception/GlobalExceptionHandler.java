60ef825cf5100b607a78aeac25149eeae22c9136
Admin <admin@>
2026-07-16 22:14:52 +0200

    fix code style

--- src/main/java/eu/babych/winelibrary/exception/GlobalExceptionHandler.java
+++ src/main/java/eu/babych/winelibrary/exception/GlobalExceptionHandler.java
@@ -50,7 +50,8 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @ExceptionHandler({UserAlreadyExistsRegistrationException.class})
-    protected ResponseEntity<Object> handleRegistrationException(UserAlreadyExistsRegistrationException ex) {
+    protected ResponseEntity<Object> handleRegistrationException(
+            UserAlreadyExistsRegistrationException ex) {
         return buildErrorResponse(ex, HttpStatus.BAD_REQUEST);
     }
 
