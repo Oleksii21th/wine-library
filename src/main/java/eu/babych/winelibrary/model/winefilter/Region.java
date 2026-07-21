1576678567e86ac51f1c6012623a779f5596f6d9
Admin <admin@>
2026-07-21 19:30:39 +0200

    add entity for Country, Region and Grape.

---
+++ src/main/java/eu/babych/winelibrary/model/winefilter/Region.java
@@ -0,0 +1,30 @@
+package eu.babych.winelibrary.model.winefilter;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "regions")
+@Getter
+@Setter
+public class Region {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "country_id")
+    private Country country;
+}
