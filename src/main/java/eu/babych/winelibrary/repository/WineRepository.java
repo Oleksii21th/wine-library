f74a50ffb6eff6122a55346573741f5ba541cbbb
Admin <admin@>
2026-07-28 16:33:33 +0200

    fix n+1 problem

--- src/main/java/eu/babych/winelibrary/repository/WineRepository.java
+++ src/main/java/eu/babych/winelibrary/repository/WineRepository.java
@@ -3,6 +3,7 @@ package eu.babych.winelibrary.repository;
 import eu.babych.winelibrary.model.wine.Wine;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -11,5 +12,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface WineRepository extends JpaRepository<Wine, Long>, JpaSpecificationExecutor<Wine> {
     @EntityGraph(attributePaths = {"country", "region", "producer", "grapes"})
-    Page<Wine> findAll(Pageable pageable);
+    Page<Wine> findAll(Specification<Wine> specification, Pageable pageable);
 }
