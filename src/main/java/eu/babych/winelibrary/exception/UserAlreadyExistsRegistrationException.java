781cf51a1419c2d679065ae33a27e94062c36126
Admin <admin@>
2026-07-28 16:37:18 +0200

    handle UserAlreadyExistsRegistrationException.

--- src/main/java/eu/babych/winelibrary/exception/UserAlreadyExistsRegistrationException.java
+++ src/main/java/eu/babych/winelibrary/exception/UserAlreadyExistsRegistrationException.java
@@ -1,6 +1,8 @@
 package eu.babych.winelibrary.exception;
 
-public class UserAlreadyExistsRegistrationException extends RuntimeException {
+import eu.babych.winelibrary.exception.badrequest.BadRequestException;
+
+public class UserAlreadyExistsRegistrationException extends BadRequestException {
     public UserAlreadyExistsRegistrationException(String email) {
         super(String.format("User with email %s already exists", email));
     }
